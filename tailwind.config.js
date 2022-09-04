001e2e622122fdfa6762c93c07db6b7ec0e83e0f
naijatechgirle <kellyeluagu@gmail.com>
2022-09-04 18:12:46 +0100

    added default styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '3rem',
+      },
+    },
   },
   plugins: [],
 }
